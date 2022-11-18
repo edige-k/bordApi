76a886d6632c658bb797fc21e990d7ee9bc8c00d
edige-k <edige-k@mail.ru>
2022-11-18 12:13:16 +0600

    without Permissions

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+class AuthController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum', ['except' => ['login','register']]);
+    }
+    public function login(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(),
+                [
+                    'email' => 'required|email',
+                    'password' => 'required'
+                ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken("API TOKEN")->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            //Validated
+            $validateUser = Validator::make($request->all(),
+                [
+                    'name' => 'required',
+                    'email' => 'required|email|unique:users,email',
+                    'password' => 'required|string|min:6|confirmed',
+                ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $token = $user->createToken("API TOKEN")->plainTextToken;
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+    /**
+     * Login The User
+     * @param Request $request
+     * @return User
+     */
+}
